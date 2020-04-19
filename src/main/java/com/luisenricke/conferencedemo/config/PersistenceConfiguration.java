5cc078efc09d93d396d0a8c02cafd3d2c2e7b85b
Luis Villalobos <luisevm24@outlook.com>
2020-04-19 17:59:54 -0500

    Fix configurations to support postgrest

--- src/main/java/com/luisenricke/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/luisenricke/conferencedemo/config/PersistenceConfiguration.java
@@ -10,11 +10,11 @@ import javax.sql.DataSource;
 @Configuration
 public class PersistenceConfiguration {
 
-    @Bean
-    public DataSource dataSource() {
-        System.out.println("My custom data source bean has been init and set");
-        return DataSourceBuilder.create()
-                .url("jdbc:mysql://localhost:3306/conference_demo?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC")
-                .build();
-    }
+//    @Bean
+//    public DataSource dataSource() {
+//        System.out.println("My custom data source bean has been init and set");
+//        return DataSourceBuilder.create()
+//                .url("jdbc:mysql://localhost:3306/conference_demo?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC")
+//                .build();
+//    }
 }
